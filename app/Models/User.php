56fe4c8fe58575ce62d91d25be86478e595d1495
Josue Meraz <josuemeraz7@gmail.com>
2026-03-14 11:03:29 -0600

    Update User.php
    Set guard_name=web on User model to fix Spatie Permission with auth:sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
     use TwoFactorAuthenticatable;
     use HasRoles;
 
+    protected string $guard_name = 'web';
+
     /**
      * The attributes that are mass assignable.
      *
